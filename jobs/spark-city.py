7573c0d6b6595afeb9fc864f2e3aa79f0d1b6527
alejlatorre <alej669@gmail.com>
2024-02-25 21:24:34 -0500

    build[docker]: add dockerfile, update services and dependencies

--- jobs/spark-city.py
+++ jobs/spark-city.py
@@ -21,9 +21,9 @@ def main():
         SparkSession.builder.appName("SmartCityStreaming")
         .config(
             "spark.jars.packages",
-            "org.apache.spark:spark-sql-kafka-0-10_2.13:3.5.0,"
-            + "org.apache.hadoop:hadoop-aws:3.3.1,"
-            + "com.amazonaws:aws-java-sdk:1.11.469",
+            "org.apache.spark:spark-sql-kafka-0-10_2.12:3.5.1,"
+            + "org.apache.hadoop:hadoop-aws:3.3.6,"
+            + "com.amazonaws:aws-java-sdk:1.12.666",
         )
         .config(
             "spark.hadoop.fs.s3a.impl",
